f63a8a933e595902bfcd3a89b40912a6156c1984
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2019-12-26 19:19:17 -0300

    fixing bug retry

--- config/routes.js
+++ config/routes.js
@@ -106,9 +106,8 @@ module.exports = function(app, passport) {
     let username = (req.user && req.user.username) || process.env.PUBLIC_USER;
     User.getUserByUsername(username, (err, user) => {
       user.populate('bookedDates', (err, fullUser) => {
-        let dateOffset = 24 * 60 * 60 * 1000 * 2; //2 days
-        let now = new Date() - dateOffset;
-        now.setTime(now.getTime() - dateOffset);
+        // let dateOffset = 24 * 60 * 60 * 1000 * 2; //2 days
+        let now = new Date(new Date().setDate(new Date().getDate() - 2));
         let nextDates = fullUser.bookedDates.filter(el => {
           return (
             el.date > now &&
