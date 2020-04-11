c46eec98c5e73e4a17c54430139aab178b5870ec
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2020-04-11 15:42:12 -0300

    Increase instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,10 +7,10 @@ module.exports = {
       script: 'server.js',
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-      instances: 2,
+      instances: 3,
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G'
+      max_memory_restart: '2G'
     }
   ]
 };
